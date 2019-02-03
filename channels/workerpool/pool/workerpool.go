cf878ecf8398771b2b9061817d79e4433f8b264a
Kirk Patton <kirk_patton@yahoo.com>
2019-02-03 13:45:12 -0800

    Update workerpool.go

--- channels/workerpool/pool/workerpool.go
+++ channels/workerpool/pool/workerpool.go
@@ -47,6 +47,7 @@ func (w *Worker) Start() {
 		case work = <-w.todo:
 			fmt.Printf("TODO: Goto work: %s\n", work.Before)
 		case <-w.stop:
+			fmt.Printfln("worker told to stop")
 			stop = true
 		}
 	}
